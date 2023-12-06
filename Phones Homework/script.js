150413f8a1d8accaf74e459f4ad95a0d3b58ec54
Viktor Zdravkovski <viktorzdravkovski5@gmail.com>
2023-12-06 22:55:47 +0100

    Telefoni Domashna

---
+++ Phones Homework/script.js
@@ -0,0 +1,21 @@
+// let Telefoni = 30;
+// let Cena = 119.95;
+// let ddv = 5;
+
+// let CenaSoDDV = Telefoni + ((Telefoni * ddv) / 100);
+
+// let SiteTelefoni = CenaSoDDV * Cena;
+
+// console.log(SiteTelefoni);
+
+// kolku shto proveriv isti rezultat davaat i dvete
+
+let Telefoni = 30;
+let Cena = 119.95;
+let ddv = 5;
+
+let CenaSoDDV = Cena + ((Cena * ddv) / 100);
+
+let SiteTelefoni = CenaSoDDV * Telefoni;
+
+console.log(SiteTelefoni);
